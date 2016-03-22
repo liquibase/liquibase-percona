97c239125463d7fbda777caea2da5daa7f052628
Andreas Dangel <adangel@users.sourceforge.net>
2016-03-22 20:02:18 +0100

    Add more unit tests References #2

--- src/test/java/liquibase/ext/percona/PerconaCreateIndexChangeTest.java
+++ src/test/java/liquibase/ext/percona/PerconaCreateIndexChangeTest.java
@@ -14,6 +14,8 @@ package liquibase.ext.percona;
  * limitations under the License.
  */
 
+import java.io.StringWriter;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -23,8 +25,11 @@ import liquibase.database.Database;
 import liquibase.database.DatabaseConnection;
 import liquibase.database.core.MySQLDatabase;
 import liquibase.executor.ExecutorService;
+import liquibase.executor.LoggingExecutor;
 import liquibase.executor.jvm.JdbcExecutor;
 import liquibase.statement.SqlStatement;
+import liquibase.statement.core.CommentStatement;
+import liquibase.statement.core.CreateIndexStatement;
 
 public class PerconaCreateIndexChangeTest
 {
@@ -57,14 +62,61 @@ public class PerconaCreateIndexChangeTest
         
     }
 
-    @Test
-    public void testCreateNewIndexReal() {
-        SqlStatement[] statements = c.generateStatements(database);
+    private void assertPerconaChange(SqlStatement[] statements, String alter) {
         Assert.assertEquals(1, statements.length);
         Assert.assertEquals(PTOnlineSchemaChangeStatement.class, statements[0].getClass());
-        Assert.assertEquals("pt-online-schema-change --alter=\"ADD UNIQUE INDEX theIndexName (indexedColumn)\" "
+        Assert.assertEquals("pt-online-schema-change --alter=\"" + alter + "\" "
                 + "--alter-foreign-keys-method=auto "
                 + "--host=localhost --port=3306 --user=user --password=*** --execute D=testdb,t=person",
                 ((PTOnlineSchemaChangeStatement)statements[0]).printCommand(database));
+    }
+
+    @Test
+    public void testCreateNewIndexReal() {
+        SqlStatement[] statements = c.generateStatements(database);
+        assertPerconaChange(statements, "ADD UNIQUE INDEX theIndexName (indexedColumn)");
+    }
+
+    @Test
+    public void testCreateIndexNonUnique() {
+        c.setUnique(false);
+        SqlStatement[] statements = c.generateStatements(database);
+        assertPerconaChange(statements, "ADD INDEX theIndexName (indexedColumn)");
+    }
+
+    @Test
+    public void testCreateIndexMultipleColumns() {
+        AddColumnConfig column2 = new AddColumnConfig();
+        column2.setName("otherColumn");
+        c.addColumn(column2);
+        SqlStatement[] statements = c.generateStatements(database);
+        assertPerconaChange(statements, "ADD UNIQUE INDEX theIndexName (indexedColumn, otherColumn)");
+    }
+
+    @Test
+    public void testCreateIndexColumnWithType() {
+        AddColumnConfig column = new AddColumnConfig();
+        column.setName("otherIntColumn");
+        column.setType("INT");
+        c.getColumns().clear();
+        c.addColumn(column);
+        SqlStatement[] statements = c.generateStatements(database);
+        assertPerconaChange(statements, "ADD UNIQUE INDEX theIndexName (otherIntColumn)");
+    }
+
+    @Test
+    public void testUpdateSQL() {
+        ExecutorService.getInstance().setExecutor(database, new LoggingExecutor(null, new StringWriter(), database));
+
+        SqlStatement[] statements = c.generateStatements(database);
+        Assert.assertEquals(3, statements.length);
+        Assert.assertEquals(CommentStatement.class, statements[0].getClass());
+        Assert.assertEquals("pt-online-schema-change --alter=\"ADD UNIQUE INDEX theIndexName (indexedColumn)\" "
+                + "--alter-foreign-keys-method=auto "
+                + "--host=localhost --port=3306 --user=user --password=*** --execute D=testdb,t=person",
+                ((CommentStatement)statements[0]).getText());
+        Assert.assertEquals(CommentStatement.class, statements[1].getClass());
+        Assert.assertEquals(CreateIndexStatement.class, statements[2].getClass());
+    }
     }
 }
