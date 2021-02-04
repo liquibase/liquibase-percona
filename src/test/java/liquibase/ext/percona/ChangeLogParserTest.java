a8a497024efd99f73b4dfffbba0e1f0e920b18cd
Andreas Dangel <andreas.dangel@adangel.org>
2021-02-04 17:33:53 +0100

    Fix tests with resolving namespace schema

--- src/test/java/liquibase/ext/percona/ChangeLogParserTest.java
+++ src/test/java/liquibase/ext/percona/ChangeLogParserTest.java
@@ -16,6 +16,9 @@ package liquibase.ext.percona;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -23,10 +26,13 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import liquibase.Scope;
+import liquibase.Scope.ScopedRunnerWithReturn;
 import liquibase.change.Change;
 import liquibase.changelog.ChangeLogParameters;
 import liquibase.changelog.ChangeSet;
 import liquibase.changelog.DatabaseChangeLog;
+import liquibase.exception.LiquibaseException;
 import liquibase.parser.ChangeLogParser;
 import liquibase.parser.ChangeLogParserFactory;
 import liquibase.resource.ResourceAccessor;
@@ -40,17 +46,43 @@ public class ChangeLogParserTest {
     @BeforeEach
     public void setup() throws IOException {
         Map<String, String> data = new HashMap<String, String>();
-        String[] files = new String[] { "test-changelog.xml", "test-changelog.yaml" };
-        for (String filename : files) {
-            data.put(filename, FileUtil.getContents(new File("src/test/resources/liquibase/ext/percona/changelog/" + filename)));
-        }
+
+        data.put("test-changelog.xml",
+                FileUtil.getContents(new File("src/test/resources/liquibase/ext/percona/changelog/test-changelog.xml")));
+        data.put("test-changelog.yaml",
+                FileUtil.getContents(new File("src/test/resources/liquibase/ext/percona/changelog/test-changelog.yaml")));
+        data.put("raw.githubusercontent.com/liquibase/liquibase-percona/liquibase-percona-2.0.0/src/main/resources/dbchangelog-ext-liquibase-percona.xsd",
+                FileUtil.getContents(new File("src/main/resources/dbchangelog-ext-liquibase-percona.xsd")));
+        data.put("www.liquibase.org/xml/ns/dbchangelog/dbchangelog-4.2.xsd",
+                readLiquibaseSchema("www.liquibase.org/xml/ns/dbchangelog/dbchangelog-4.2.xsd"));
+
         resourceAccessor = new MockResourceAccessor(data);
     }
-    
+
+    private static String readLiquibaseSchema(String path) throws IOException {
+        char[] buffer = new char[8192];
+        StringBuilder sb = new StringBuilder(66000);
+        try (Reader in = new InputStreamReader(ChangeLogParserTest.class.getResourceAsStream("/" + path), StandardCharsets.UTF_8)) {
+            int count = in.read(buffer);
+            while (count > 0) {
+                sb.append(buffer, 0, count);
+                count = in.read(buffer);
+            }
+        }
+        return sb.toString();
+    }
+
     private DatabaseChangeLog loadChangeLog(String filename) throws Exception {
-        ChangeLogParserFactory parserFactory = ChangeLogParserFactory.getInstance();
-        ChangeLogParser parser = parserFactory.getParser(filename, resourceAccessor);
-        return parser.parse(filename, new ChangeLogParameters(), resourceAccessor);
+        Map<String, Object> objects = new HashMap<>();
+        objects.put(Scope.Attr.resourceAccessor.name(), resourceAccessor);
+        return Scope.child(objects,
+                new ScopedRunnerWithReturn<DatabaseChangeLog>() {
+                public DatabaseChangeLog run() throws LiquibaseException {
+                    ChangeLogParserFactory parserFactory = ChangeLogParserFactory.getInstance();
+                    ChangeLogParser parser = parserFactory.getParser(filename, resourceAccessor);
+                    return parser.parse(filename, new ChangeLogParameters(), resourceAccessor);
+            }
+        });
     }
 
     @Test
