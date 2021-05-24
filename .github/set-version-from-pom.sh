8520fe0bc29ac125c3c01c1051699a6716f2dc65
Andreas Dangel <andreas.dangel@adangel.org>
2021-05-24 11:38:51 +0200

    Update gh actions workflows
    - use maven wrapper (./mvnw)
    - use batch mode (-B)
    - integration tests run via profile
    - update project.build.outputTimestamp for releases

--- .github/set-version-from-pom.sh
+++ .github/set-version-from-pom.sh
@@ -3,4 +3,4 @@
 set -e
 set -o pipefail
 
-echo "VERSION_TAG=$(mvn org.apache.maven.plugins:maven-help-plugin:3.1.0:evaluate -Dexpression=project.version -q -DforceStdout)" >> $GITHUB_ENV
+echo "VERSION_TAG=$(./mvnw -B org.apache.maven.plugins:maven-help-plugin:3.1.0:evaluate -Dexpression=project.version -q -DforceStdout)" >> $GITHUB_ENV
